41fde04062070b70689e4343b4fae77e24310d12
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-27 19:18:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
 def my_each(codes) # put argument(s) here
   # code here
-  while codes < 50
-    puts "Told you so!"
-  yield(50)
-  puts "This is the #{num} phrase"
-end
+  i = 0
+  while i < codes.length
+    yield(codes[i])
+  i = i + 1
+  end
+  codes
+end   
 
-my_each { |codes| puts "#{num}"}
+
+
