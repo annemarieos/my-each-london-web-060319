ffa7412e6998fb2833ed85495f2de13a25671412
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-27 19:15:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(codes) # put argument(s) here
   # code here
+  while codes < 50
+    puts "Told you so!"
+  yield(50)
+  puts "This is the #{num} phrase"
 end
+
+my_each { |codes| puts "#{num}"}
